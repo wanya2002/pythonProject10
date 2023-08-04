01c6553ec9380aec8808ace9b2c66831ec0e310a
wanya2002 <wanya2002@yandex.ru>
2023-08-04 16:09:19 +0300

    commit1

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from main.views import index
+from main.views import index, contacts
 
 urlpatterns =[
     path('', index)
+]
+
+urlpatterns = [
+    path('', contacts)
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
    return render(request, 'main/index.html')
+
+
+def contacts(request):
+   return render(request, 'main/contacts.html')
